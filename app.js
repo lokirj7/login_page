2aff6c3b0f86cac592f909a66d7bb46b625805a0
lokirj7 <iamloki765@gmail.com>
2023-06-18 22:57:38 +0530

    secure and encryp database

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
+require('dotenv').config();
 const express=require('express');
 const bodyParser = require('body-parser');
 const ejs =require('ejs');
-const mongoose =require('mongoose');
-
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 const app = express();
 
-app.use(express.static("public"));app.set('view engine','ejs');
+
+console.log(process.env.SECRET);
+app.use(express.static("public"));
+app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
 //Here i'm going to register username,password in db
 mongoose.connect("mongodb://127.0.0.1:27017/userdb", { useNewUrlParser: true  });
 
 
-const user = {
+const user = new mongoose.Schema ({
   Email: String,
   Password: String
-};
+});
+
+user.plugin(encrypt, { secret:process.env.SECRET,encryptedFields: ['Password'] });
 
 const usecoll =new mongoose.model("user", user);
 
@@ -28,7 +34,7 @@ app.get("/register",function(req,res){
 })
 app.get("/login",function(req,res){
     res.render("login");
-})
+} )
 
 
 app.post("/register",function(req,res){
@@ -45,8 +51,26 @@ app.post("/register",function(req,res){
       console.log(err);
     });
   
-})
+});
 
+ app.post("/login",function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+user.findOne({Email:username},function(err,founduser){
+  if(err){
+    console.log(err);
+  }
+  else{
+    if(founduser){
+      if(founduser.password === password){
+        res.render("secrets");
+      }
+    }
+  }
+});
+  
+
+ });
 
 app.listen(3001,function(){
     console.log("Server started on port 3000");
